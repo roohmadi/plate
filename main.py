b1406c9275123b41f1c3468e5d600359b39351e2
roohmadi <roohmadi@gmail.com>
2024-07-04 20:45:58 +0700

    .

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ coco_model = YOLO('yolov8n.pt')
 license_plate_detector = YOLO('license_plate_detector.pt')
 
 # load video
-cap = cv2.VideoCapture('./sample.mp4')
+cap = cv2.VideoCapture('./mini.mp4')
 
 vehicles = [2, 3, 5, 7]
 
@@ -47,7 +47,7 @@ while ret:
             xcar1, ycar1, xcar2, ycar2, car_id = get_car(license_plate, track_ids)
 
             if car_id != -1:
-                cv2.imwrite('last_Img.jpg', frame)
+                
 
                 # crop license plate
                 license_plate_crop = frame[int(y1):int(y2), int(x1): int(x2), :]
@@ -58,8 +58,12 @@ while ret:
 
                 # read license plate number
                 license_plate_text, license_plate_text_score = read_license_plate(license_plate_crop_thresh)
+                
 
                 if license_plate_text is not None:
+                    if (license_plate_text != ''):
+                        cv2.imwrite(license_plate_text + '_Img.jpg', frame)
+                        cv2.imwrite(license_plate_text + '_crop_Img.jpg', license_plate_crop)
                     results[frame_nmr][car_id] = {'car': {'bbox': [xcar1, ycar1, xcar2, ycar2]},
                                                   'license_plate': {'bbox': [x1, y1, x2, y2],
                                                                     'text': license_plate_text,
